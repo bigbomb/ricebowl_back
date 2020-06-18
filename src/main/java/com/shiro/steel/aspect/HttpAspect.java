ee9494ffbbc87823fbbdf08b62632ecaa9d16a6d
Lu Junjie1 陆俊杰(SOS) <pfiamd@sos.shanghaigm.com>
2020-06-18 12:28:59 +0800

    更新

--- src/main/java/com/shiro/steel/aspect/HttpAspect.java
+++ src/main/java/com/shiro/steel/aspect/HttpAspect.java
@@ -1,11 +1,12 @@
 package com.shiro.steel.aspect;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.mysql.jdbc.log.LogFactory;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -45,7 +46,7 @@ public class HttpAspect extends BaseApi{
     @Autowired
     private OperatingRecordService operatingRecordService;
 
-    private final static Logger log = LogManager.getLogger(HttpAspect.class);
+    private final static Logger log = LoggerFactory.getLogger(HttpAspect.class);
 
     @Pointcut("execution(public * com.shiro.steel.api..*(..))")
     public void log() {
